b658e643db9c5daf4548f9c043f1b06807daca6e
Daniil <favdela4tune@gmail.com>
2022-05-17 11:41:06 +0300

    room added

--- app/src/main/java/db/SetDataEntity.kt
+++ app/src/main/java/db/SetDataEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "set_table")
-class SetDataEntity {
+data class SetDataEntity (
     @PrimaryKey(autoGenerate = true) val id: Int,
     val set: Set
-}
+)
